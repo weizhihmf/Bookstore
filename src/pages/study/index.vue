37a57b0333ee1fb49c2391e1703a170171b93c39
3021715895@qq.com <3021715895@qq.com>
2020-01-05 08:15:51 +0800

    update file study

--- src/pages/study/index.vue
+++ src/pages/study/index.vue
@@ -25,29 +25,20 @@
       <i-tab key="0" title="学习记录"></i-tab>
       <i-tab key="1" title="离线课程"></i-tab>
   </i-tabs>
-  <swiper @change="changeView" :current="current" style="height:1150px;">
-      <swiper-item v-for="(item,index1) in vedioUrl" :key="index1">
-       <i-grid v-for="value in item" :key="value">
-         <view>
-             <video :src="value.url" style="height:130px;"></video>
+  <swiper @change="changeView" :current="current" style="height:450px;">
+      <swiper-item v-for="(item,index1) in vedioList" :key="index1">
+       <!-- <i-grid v-for="value in item" :key="value"> -->
+         <!-- <view v-for="value in item" :key="value"> -->
+             <!-- <video :src="value.url" style="height:130px;"></video>
           <i-panel :title="value.remark">
-      </i-panel> 
-         </view> 
-    </i-grid> 
+      </i-panel>  -->
+          <i-card v-for="value in item" :key="value" title="value.title" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+          <view slot="value.remark">内容不错</view>
+          </i-card>
+         <!-- </view>  -->
+    <!-- </i-grid>  -->
     </swiper-item>
   </swiper>
-  <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-    <view slot="content">内容不错</view>
-    <view slot="footer">尾部内容</view>
-</i-card>
-<i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-    <view slot="content">内容不错</view>
-    <view slot="footer">尾部内容</view>
-</i-card>
-<i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-    <view slot="content">内容不错</view>
-    <view slot="footer">尾部内容</view>
-</i-card>
   </div>
 </template>
 
@@ -66,6 +57,14 @@ export default {
         'cloud://hubuhmf-nzru0.6875-hubuhmf-nzru0-1300842120/images/read.png'
       ],
       grids: [],
+      vedioList:[
+      [
+        {title:"视频一",remark:"介绍1",url:"	cloud://hubuhmf-nzru0.6875-hubuhmf-nzru0-1300842120/vedioUrl/1.mp4"},
+      ]
+      [  
+        {title:"视频2",remark:"介绍2",url:"	cloud://hubuhmf-nzru0.6875-hubuhmf-nzru0-1300842120/vedioUrl/10.mp4"}
+      ]
+      ]
     }
   },
   created() {
@@ -79,17 +78,20 @@ export default {
   },
   methods: {
      handleChangeScroll(type){
-        this.current = type.mp.detail.key
+      this.current = type.mp.detail.key
+    },
+    changeView(e){
+      this.current = e.mp.detail.current
     },
      goUrl (title) {
-       if(title==="笔记"){
-         mpvue.navigateTo({ url:'../note/main' })
+      if(title==="笔记"){
+        mpvue.navigateTo({ url:'../note/main' })
        }
-        if(title==="收藏"){
-         mpvue.navigateTo({ url:'../save/main' })
+      if(title==="收藏"){
+        mpvue.navigateTo({ url:'../save/main' })
        }
-       if(title==="单词本"){
-         mpvue.navigateTo({ url:'../word/main' })
+      if(title==="单词本"){
+        mpvue.navigateTo({ url:'../word/main' })
        }   
     },
     goTo(){
