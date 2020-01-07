426734379dd6c793d56f1deadb1b645928b6e079
3021715895@qq.com <3021715895@qq.com>
2020-01-07 08:34:33 +0800

    update file word

--- src/pages/word/index.vue
+++ src/pages/word/index.vue
@@ -6,6 +6,7 @@
             <i-cell title="discharge\n  v.准许（某人）离开；释放；排出；放电；"></i-cell>
             <i-cell title="dignity\n  n.尊严；高贵；"></i-cell>
             <i-cell title="muddy\n  adj.泥泞的；模糊的；混乱的；"></i-cell>
+            <i-input :value="value" autofocus placeholder="写下你的学习单词吧" @click="addWords"/>
             </i-cell-group>
         </i-panel>
     </div>
@@ -15,7 +16,13 @@
 export default {
     data() {
         return {
-            
+            value:''
+        }
+    },
+    methods: {
+        addWords(e){
+            console.log(e)
+            this.value=e.mp.detail.value
         }
     },
 }
